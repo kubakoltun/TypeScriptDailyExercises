b0278581ad45494629b6cf37cff862596f2cdddb
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-08 01:09:10 +0200

    Update detect_pangram.ts

--- detect_pangram.ts
+++ detect_pangram.ts
@@ -1,31 +1,26 @@
 export const isPangram = (phrase: string): boolean => {
   const lettersInAlphabet: number = 26;
-  let occurencesOfLet: number = 0;
   let uniqueLet: number = 0;
   
   for (let i = 0; i < phrase.length; i++) {
-    occurencesOfLet = 0;
     if (phrase[i].match(/[ 1234567890`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/)) {
       //{}
     } else {
-      for (let j = 0; j < phrase.length; j++) {
-        if (phrase[i] == phrase[j]) {
-          console.log(` i ${phrase[i]} j ${phrase[j]} `)
-          occurencesOfLet++;
-          
+      let found: boolean = false;
+      for (let j = 0; j < i; j++) {
+        if (phrase[i].toLowerCase() == phrase[j].toLowerCase()) {
+          found = true;
+          break;
         }
       }
-      
+      if (!found) {
+        uniqueLet++;
+      }
     }
-    console.log(occurencesOfLet);
-    if (occurencesOfLet == 1) {
-      uniqueLet++;
+    if (uniqueLet == lettersInAlphabet) {
+      return true;
     }
   }
-  console.log(uniqueLet);
-  if (uniqueLet == lettersInAlphabet) {
-    return true;
-  } else {
-    return false;
-  }
+  
+  return false;
 }
