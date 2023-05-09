71b37497e5b52148748ff36d96686ab627993578
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-09 19:25:19 +0200

    Create reverse_words.ts

---
+++ reverse_words.ts
@@ -0,0 +1,13 @@
+export function spinWords(words: string): string {
+    let wordsList: string[] = words.split(" ");
+    
+    for (let i = 0; i < wordsList.length; i++) {
+      if (wordsList[i].length >= 5) {
+        let letters: string[] = wordsList[i].split("");
+        let revLetters: string = letters.reverse().join("");
+        wordsList[i] = revLetters;
+      }
+    }
+  
+  return wordsList.join(" ");
+}
