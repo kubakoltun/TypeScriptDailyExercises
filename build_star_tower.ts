d46043c53e1c74d21f8971674a62fb56f4b1b9f7
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-09 19:05:22 +0200

    Create build_star_tower.ts

---
+++ build_star_tower.ts
@@ -0,0 +1,13 @@
+export const towerBuilder = (nFloors: number): string[] => {
+  let tower: string[] = [];
+  let sides: number = 1;
+  for (let i = 0; i < nFloors; i++) {
+    tower[i] = "";
+    if (i < nFloors-1) tower[i] += " ".repeat((nFloors-(i+1)));
+    tower[i] += "*".repeat(sides);
+    if (i < nFloors-1) tower[i] += " ".repeat((nFloors-(i+1)));
+    sides += 2;
+  }
+  
+  return tower;
+}
