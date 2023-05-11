9f993d0df0bf2b35e5a656bddd3262c83ebceb60
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-11 15:08:10 +0200

    Create highest_scoring_word.ts

---
+++ highest_scoring_word.ts
@@ -0,0 +1,19 @@
+export const high = (str: string): string => {
+  let word: string[] = str.split(" ");
+  let sum: number = 0;
+  let maxSum: number = 0;
+  let result: string = "";
+  
+  for (let i = 0; i < word.length; i++) {
+    sum = 0;
+    for (let j = 0; j < word[i].length; j++) {
+      sum += word[i].charCodeAt(j) - 96;
+    }
+    if (sum > maxSum) {
+      maxSum = sum;
+      result = word[i];
+    }
+  }
+  
+  return result;
+}
