7dac737f9121ad970650c12d659c1db2b8b9ea5a
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-17 19:09:36 +0200

    Update v2_catches_up_to_v1.ts

--- v2_catches_up_to_v1.ts
+++ v2_catches_up_to_v1.ts
@@ -8,8 +8,8 @@ export const race = (v1:number, v2:number, g:number) => {
   
   let eliminateGap: number = g/additionalSpped;
   let h: number = Math.floor(eliminateGap);
-  let m: number = Math.floor((eliminateGap-h)*60);
-  let s: number = Math.floor((((eliminateGap - h)*60)-m)*60);
+  let m: number = Math.floor((eliminateGap * 60) % 60);
+  let s: number = Math.floor((eliminateGap * 3600) % 60);
   
   timeToCatchUp[0] = h;
   timeToCatchUp[1] = m;
