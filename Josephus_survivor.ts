be7a31f81c9be0511a9f81954b01b0dee7a6684a
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-20 14:09:54 +0200

    Create Josephus_survivor.ts

---
+++ Josephus_survivor.ts
@@ -0,0 +1,24 @@
+export function josephusSurvivor(n: number, k: number) {
+  let downSize: number = n;
+  let kStep: number = 1;
+  let i: number = 0;
+  let nTab: number[] = Array.from({ length: n }, (value, index) => index+1);
+  
+  while (nTab.length > 1) {
+    if (kStep === k) {
+      kStep = 0;
+      nTab.splice(i, 1);
+      i--;
+    }
+    kStep++;
+    i++;
+    if (i > nTab.length-1) {
+      i = 0;
+    }
+    if(nTab.length === 1) {
+      break;
+    }
+  }
+  
+  return nTab[0];
+}
