c7d345f9cef178590aa061818cc5a190ee78510f
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-05-31 20:15:22 +0200

    Update array_diff.ts

--- array_diff.ts
+++ array_diff.ts
@@ -1,19 +1,4 @@
 export function arrayDiff(a: number[], b: number[]): number[] {
-  let newArr: number[] = [];
-  if (b.length === 0) {
-    newArr = a;
-  }
-  for (let i = 0; i < b.length; i++) {
-      if (a.includes(b[i])) {
-        let g: number = 0;
-        for (let j = 0; j < a.length; j++) {
-          if (a[j] !== b[i]) {
-            newArr[g] = a[j];
-            g++;
-          } 
-        }
-      }  
-  }
-                                      
+  let newArr = a.filter(num => !b.includes(num));                 
   return newArr;
 }
