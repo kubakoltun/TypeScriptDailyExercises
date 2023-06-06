956f621055c377f62981606c6633a0ad6614e542
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-06 19:20:50 +0200

    Update cyclists_training.ts

--- cyclists_training.ts
+++ cyclists_training.ts
@@ -1,21 +1,21 @@
-export function temps(v0:number, slope:number, dTot:number): number {
-  const GRAVITY_ACC = 9.81 * 3.6 * 60.0;    
-  const DRAG = 60.0 * 0.3 / 3.6;             
-  const DELTA_T = 1.0 / 60.0;                
-  const G_THRUST = 60 * 3.6 * 3.6;          
-  const MASS = 80.0;                         
-  const WATTS0 = 225.0;                     
-  const D_WATTS = 0.5; 
-  
+export function temps(v0: number, slope: number, dTot: number): number {
+  const GRAVITY_ACC = 9.81 * 3.6 * 60.0;
+  const DRAG = 60.0 * 0.3 / 3.6;
+  const DELTA_T = 1.0 / 60.0;
+  const G_THRUST = 60 * 3.6 * 3.6;
+  const MASS = 80.0;
+  const WATTS0 = 225.0;
+  const D_WATTS = 0.5;
+
   let t = 0;
   let gamma = 0;
   let v = v0;
   let d = 0;
   let watts = WATTS0;
-  
+
   while (d < dTot) {
     const slopeFactor = Math.atan(slope / 100); 
-    const slopeEffect = GRAVITY_ACC * Math.tan(slopeFactor); 
+    const slopeEffect = GRAVITY_ACC * Math.sin(slopeFactor);
 
     const acceleration = -slopeEffect - (DRAG * Math.abs(v) * v / MASS);
 
@@ -31,7 +31,8 @@ export function temps(v0:number, slope:number, dTot:number): number {
     }
 
     v = v + gamma * DELTA_T;
-    d = d + (v * DELTA_T) / 60.0;
+    d = d + (v * DELTA_T);
+
     watts = watts - (D_WATTS * DELTA_T);
 
     if (v - 3.0 <= 1e-2) {
