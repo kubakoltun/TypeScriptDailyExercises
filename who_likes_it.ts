7bf1e90d7aaa7b98069ea6badf3061825b66bb3a
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-08 12:03:16 +0200

    Create who_likes_it.ts

---
+++ who_likes_it.ts
@@ -0,0 +1,19 @@
+export const likes = (a : string[]) : string => {
+  console.log(a);
+  console.log(a.length);
+  let whoLikesIt: string = "";
+  if (a.length === 0) {
+    return "no one likes this";
+  }
+  else if (a.length == 1) {
+    return `${a[0]} likes this`;
+  }
+  else if (a.length == 2) {
+    return `${a[0]} and ${a[1]} like this`;
+  }
+  else if (a.length == 3) {
+    return `${a[0]}, ${a[1]} and ${a[2]} like this`;
+  } else {
+    return `${a[0]}, ${a[1]} and ${a.length-2} others like this`;
+  }
+}
