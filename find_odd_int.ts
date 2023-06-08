cfc94a922f2e5934f73b432096b7ffc1e224403a
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-08 12:01:55 +0200

    Create find_odd_int.ts

---
+++ find_odd_int.ts
@@ -0,0 +1,14 @@
+export const findOdd = (xs: number[]): number => {
+  let count: number = 0;
+  let n: number = 0;
+
+  xs.forEach((num) => {
+    count = xs.filter((x) => x === num).length;
+    console.log(count)
+    if (count % 2 !== 0) {
+      n = num;
+    }
+  });
+
+  return n;
+};
