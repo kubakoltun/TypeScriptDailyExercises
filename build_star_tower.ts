db64aa920def98134be3988375501ff37fa11356
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-06-14 10:03:04 +0200

    Update build_star_tower.ts

--- build_star_tower.ts
+++ build_star_tower.ts
@@ -1,6 +1,7 @@
 export const towerBuilder = (nFloors: number): string[] => {
   let tower: string[] = [];
   let sides: number = 1;
+  
   for (let i = 0; i < nFloors; i++) {
     tower[i] = "";
     if (i < nFloors-1) tower[i] += " ".repeat((nFloors-(i+1)));
