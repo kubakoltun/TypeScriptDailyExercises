1d72e066a1a8bc09edaea5d5100cb06fa913ab5c
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-22 18:46:13 +0200

    Update weird_prime_generator.ts

--- weird_prime_generator.ts
+++ weird_prime_generator.ts
@@ -2,17 +2,14 @@ function gcd(a: number, b: number): number {
   while (b !== 0) {
     [a, b] = [b, a % b];
   }
-  
   return a;
 }
 
 function an(n: number): number[] {
   const sequence: number[] = [7];
-  
   for (let i = 2; i <= n; i++) {
     sequence.push(sequence[i - 2] + gcd(i, sequence[i - 2]));
   }
-  
   return sequence;
 }
 
@@ -21,26 +18,22 @@ function gn(n: number): number[] {
   for (let i = 2; i <= n; i++) {
     sequence.push(gcd(i, sequence[i - 2]));
   }
-  
   return sequence;
 }
 
 export function countOnes(n: number): number {
   const sequence: number[] = [1, ...gn(n)];
-  
   return sequence.filter((num) => num === 1).length;
 }
 
 function p(n: number): number[] {
   const sequence: number[] = [1, ...gn(n)];
   const primes: number[] = [];
-  
   for (const num of sequence) {
     if (num !== 1 && isPrime(num)) {
       primes.push(num);
     }
   }
-  
   return primes.slice(0, n);
 }
 
@@ -48,13 +41,11 @@ function isPrime(num: number): boolean {
   if (num < 2) {
     return false;
   }
-  
   for (let i = 2; i <= Math.sqrt(num); i++) {
     if (num % i === 0) {
       return false;
     }
   }
-  
   return true;
 }
 
@@ -67,13 +58,11 @@ function anOver(n: number): number[] {
   const sequence: number[] = an(n);
   const gnSequence: number[] = gn(n);
   const result: number[] = [];
-  
   for (let i = 0; i < n; i++) {
     if (gnSequence[i] !== 1) {
       result.push(sequence[i] / (i + 1));
     }
   }
-  
   return result;
 }
 
