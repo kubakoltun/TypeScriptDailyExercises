4addd13c0007702f7404948fa07f3c4f4ac88f53
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-08-27 23:18:41 +0200

    Update cyclists_training.ts

--- cyclists_training.ts
+++ cyclists_training.ts
@@ -1,12 +1,11 @@
 export function temps(v0: number, slope: number, dTot: number): number {
- const GRAVITY_ACC = 9.81 * 3.6 * 60.0; 
+  const GRAVITY_ACC = 9.81 * 3.6 * 60.0; 
   const DRAG = 60.0 * 0.3 / 3.6; 
   const DELTA_T = 1.0 / 60.0; 
   const G_THRUST = 60 * 3.6 * 3.6; 
   const MASS = 80.0; 
   const WATTS0 = 225.0; 
   const D_WATTS = 0.5; 
-
   let t = 0; 
   let gamma = 0; 
   let v = v0; 
