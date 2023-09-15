bba7e18d0ba2c19d0e69c86558048e6a33d6873c
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-09-15 16:41:58 +0200

    Update weird_prime_generator.ts

--- weird_prime_generator.ts
+++ weird_prime_generator.ts
@@ -1,73 +1,64 @@
 function gcd(a: number, b: number): number {
-  while (b !== 0) {
-    [a, b] = [b, a % b];
-  }
-  return a;
-}
-
-function an(n: number): number[] {
-  const sequence: number[] = [7];
-  for (let i = 2; i <= n; i++) {
-    sequence.push(sequence[i - 2] + gcd(i, sequence[i - 2]));
-  }
-  return sequence;
-}
-
-function gn(n: number): number[] {
-  const sequence: number[] = [1];
-  for (let i = 2; i <= n; i++) {
-    sequence.push(gcd(i, sequence[i - 2]));
-  }
-  return sequence;
-}
-
-export function countOnes(n: number): number {
-  const sequence: number[] = [1, ...gn(n)];
-  return sequence.filter((num) => num === 1).length;
-}
-
-function p(n: number): number[] {
-  const sequence: number[] = [1, ...gn(n)];
-  const primes: number[] = [];
-  for (const num of sequence) {
-    if (num !== 1 && isPrime(num)) {
-      primes.push(num);
+    if (!b) {
+        return a;
     }
-  }
-  return primes.slice(0, n);
+    return gcd(b, a % b);
 }
 
-function isPrime(num: number): boolean {
-  if (num < 2) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      return false;
+export function an(n: number): number[] {
+    const a: number[] = [];
+    a[0] = 7;
+    if (n < 2) {
+        return a;
     }
-  }
-  return true;
-}
-
-export function maxPn(n: number): number {
-  const primes: number[] = p(n);
-  return primes.length > 0 ? Math.max(...primes) : -Infinity;
-}
-
-function anOver(n: number): number[] {
-  const sequence: number[] = an(n);
-  const gnSequence: number[] = gn(n);
-  const result: number[] = [];
-  for (let i = 0; i < n; i++) {
-    if (gnSequence[i] !== 1) {
-      result.push(sequence[i] / (i + 1));
+    for (let i = 2; i <= n; i++) {
+        const res = a[i - 2] + gcd(i, a[i-2]);
+        a[i - 1] = res;
     }
-  }
-  return result;
+    return a;
 }
 
-export function anOverAverage(n: number): number {
-  const sequence: number[] = anOver(n);
-  const sum: number = sequence.reduce((a, b) => a + b, 0);
-  return sequence.length > 0 ? Math.floor(sum / sequence.length) : NaN;
+export function gn(n: number): number[] {
+    const arr = an(n);
+    let j = 1;
+    for(let i = 0; i < arr.length - 1; i++) {
+        arr[i] = arr[j] - arr[i];
+        j++
+    }
+    arr.unshift(1);
+    return arr;
+}
+
+export function countOnes(n:number ):number {
+  return gn(n).filter(n => n == 1).length;
+}
+
+const cache = Array.from(new Set(gn(2000000).filter(num => num != 1)))
+export function p(n: number): number[] {
+    return cache.slice(0, n);
+}
+
+export function maxPn(n:number ): number {
+  return Math.max(...p(n));
+}
+
+export function anOver(n: number): number[] {
+    const arrAN: number[] = an(10000*n);
+    const arrGN: number[] = gn(10000*n);
+    const res: number[] = []
+    for (let i = 0; i < arrAN.length; i++) {
+        if (arrGN[i] != 1) {
+            res.push(arrAN[i] / (i + 1));
+        }
+        if (res.length == n) {
+            break;
+        }
+    }
+    return res;
+}
+
+export function anOverAverage(n:number):number {
+  const arr: number[] = anOver(n);
+  const arrSum: number = arr.reduce((a,b) => a + b, 0);
+  return arrSum / arr.length;
 }
