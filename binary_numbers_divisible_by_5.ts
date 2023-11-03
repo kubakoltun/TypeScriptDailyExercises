2f1e6d9cd18bb00cd978303a01ae092dbdad9d6b
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-11-03 17:16:42 +0100

    Update binary_numbers_divisible_by_5.ts

--- binary_numbers_divisible_by_5.ts
+++ binary_numbers_divisible_by_5.ts
@@ -1,8 +1,8 @@
 function divisibleByFive(binaryInput: string): boolean {
     let sum: number = 0;
     let revI: number = 0;
-    //const decimalNumber = parseInt(binaryInput, 2);
-    //decimalNumber % 5 === 0 ? return true : return false;
+    // const decimalNumber = parseInt(binaryInput, 2);
+    // decimalNumber % 5 === 0 ? return true : return false;
     
     for (let i = binaryInput.length-1; i >= 0; i--) {
         if (Number(binaryInput.charAt(i)) === 1) {
@@ -22,5 +22,5 @@ function convertioToBinary(decimalInput: number): string {
     return decimalInput.toString(2);
 }
   
-//or as a regex
+// or as a regex
 export const divisibleByFive = /^(0|1(10)*(0|11)(01*0(01)*(00|1))*1)+$/;
